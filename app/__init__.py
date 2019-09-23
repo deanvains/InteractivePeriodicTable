6cf27c36fcae628b6da36a73e29b81fafa16644e
Dean Vains <dean.vains@outlook.com>
2019-09-23 18:08:21 +0800

    trying

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 from app import routes, models
