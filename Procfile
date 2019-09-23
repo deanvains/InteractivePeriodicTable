a838c902be231f9733c38e52862790b329594f6c
Dean Vains <dean.vains@outlook.com>
2019-09-23 17:49:15 +0800

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python SamProject.py
--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
 class DevelopmentConfig(Config):
     FLASK_DEBUG=1
