78ab2541799791e44bc715571b23e4bef0457235
Dean Vains <dean.vains@outlook.com>
2019-09-24 22:49:37 +0800

    d

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,7 @@ def change(id):
         temp = ''
         if(element.description != None):
             temp = element.description
-            temp += '<br> <br>'
+            temp += ' \n \n '
         temp += form.newDesc.data
         element.description = temp
         #error coming from not being able to find element
